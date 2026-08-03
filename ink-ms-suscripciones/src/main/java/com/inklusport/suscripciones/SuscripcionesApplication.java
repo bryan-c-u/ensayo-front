90a549b960bb2ed770aa719a1b3287078c7e0f62
bryan-c-u <uranpvabryancamilo@gmail.com>
2026-08-03 08:51:46 -0500

    feat : crear modulos para trabajar

---
+++ ink-ms-suscripciones/src/main/java/com/inklusport/suscripciones/SuscripcionesApplication.java
@@ -0,0 +1,16 @@
+package com.inklusport.suscripciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+@EnableAsync
+public class SuscripcionesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuscripcionesApplication.class, args);
+    }
+}
